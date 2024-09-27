f4a6b44e23d49e6c66d805aaec1522e201673ae1
Thomas <thbasse@student.42.fr>
2024-09-27 17:51:08 +0200

    u

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: thbasse <thbasse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 11:40:03 by thbasse           #+#    #+#             */
-/*   Updated: 2024/09/27 16:36:06 by thbasse          ###   ########.fr       */
+/*   Updated: 2024/09/27 17:50:15 by thbasse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	main(int argc, char **argv)
 {
+	t_philo philo_de_tes_morts;
+	
 	if (argc < 5 || argc > 6)
 	{
 		printf("Format: ./philo <number_of_philosophers> <time_to_die> ");
@@ -22,7 +24,10 @@ int	main(int argc, char **argv)
 		return (-1);
 	}
 	if (check_is_number(argv) == -1)
+	{
+		printf("philo use ONLY numbers\n");
 		return (-1);
+	}
 	// parsing_et_initialisation(argv);
 	// partie_active();
 	return (0);
@@ -70,5 +75,7 @@ int	ft_atoi(const char *str)
 		result = result * 10 + (*str - '0');
 		str++;
 	}
+	// if (result > INT_MAX)				??????????????????????
+		// return (EXIT_FAILURE);			pas sur de ca
 	return (sign * result);
 }
