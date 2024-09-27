972fcec1841a32e7e86cb7d9ec8ef9265b32da0e
Thomas <thbasse@student.42.fr>
2024-09-27 12:06:22 +0200

    u

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: thbasse <thbasse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 11:40:03 by thbasse           #+#    #+#             */
-/*   Updated: 2024/09/24 15:19:08 by thbasse          ###   ########.fr       */
+/*   Updated: 2024/09/27 12:04:34 by thbasse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,9 @@ int	main(int argc, char **argv)
 	if (argc < 5 || argc > 6)
 	{
 		printf("Format: ./philo <number_of_philosophers> <time_to_die> ");
-		printf("time_to_eat> <time_to_sleep> ");
+		printf("<time_to_eat> <time_to_sleep> ");
 		printf("optional:[number_of_times_each_philosopher_must_eat]\n");
 	}
+	(void)argv;
 	return (0);
 }
